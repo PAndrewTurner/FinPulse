949268dfa9fe1c57ca5fccea6762d89e8e1672f9
P. Andrew Turner <Aturner326@gmail.com>
2023-11-25 21:51:33 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -1,74 +1,68 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split  # for splitting the data
-from sklearn.metrics import mean_squared_error  # for calculating the cost function
-import matplotlib.pyplot as plt
-import pickle as pkl
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
+from xgboost import XGBRegressor
+import joblib
 import warnings
+import joblib
 
-# Set numpy print options for better readability
+
 np.set_printoptions(precision=2)
-
-# Suppress warnings for better readability
 warnings.filterwarnings("ignore")
 
 # Read the Walmart store sales data from a CSV file
 data = pd.read_csv("Walmart_Store_sales.csv")
 
 # Extract features (x) and target variable (y) from the data
-x = data.drop(['Weekly_Sales', 'Date'], axis=1)  # Features
-y = data['Weekly_Sales']  # Target
-
-# Display the head of the dataset
-data.head()
+x = data.drop(['Weekly_Sales', 'Date'], axis=1)
+y = data['Weekly_Sales']
 
 # Split the data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=28)
 
 # Create and train a RandomForestRegressor model
-model = RandomForestRegressor(n_estimators=1000, random_state=28)
-model.fit(x_train, y_train)
+rf_model = RandomForestRegressor(n_estimators=1000, random_state=28)
+rf_model.fit(x_train, y_train)
 
-# Save the trained model to a file using pickle
-filename = 'model.pkl'
-pkl.dump(model, open(filename, 'wb'))
+# Save the trained RandomForestRegressor model to a file using joblib
+rf_filename = 'rf_model.joblib'
+joblib.dump(rf_model, rf_filename)
 
-# Make predictions on the test set
-y_pred = model.predict(x_test)
+print("RF Model Done")
 
-# Calculate and print the Root Mean Squared Error (RMSE)
-rmse = float(format(np.sqrt(mean_squared_error(y_test, y_pred)), '.3f'))
-print("\nRMSE: ", rmse)
+# Create and train an XGBRegressor model
+xgb_model = XGBRegressor(n_estimators=1000, random_state=28)
+xgb_model.fit(x_train, y_train)
 
-# Read the test data from a CSV file
-test_data = pd.read_csv("test_data.csv")
+# Save the trained XGBRegressor model to a file using joblib
+xgb_filename = 'xgb_model.joblib'
+joblib.dump(xgb_model, xgb_filename)
 
-# Initialize empty lists for storing predictions and dates
-vals = []
-dates = []
+print("XGB Model Done")
 
-# Extract dates from the test_data and append to the dates list
-for i in range(len(test_data)):
-    st = str(test_data['Month'].loc[i]) + "/" + str(test_data['Day'].loc[i]) + "/" + str(test_data['Year'].loc[i])
-    dates.append(st)
+gb_model = GradientBoostingRegressor(n_estimators=1000, random_state=28)
+gb_model.fit(x_train, y_train)
 
-print(dates)
+gb_filename = 'gb_model.joblib'
+joblib.dump(gb_model, gb_filename)
 
-# Define feature columns for the test data
-feature_columns = ['Store', 'Month', 'Day', 'Year', 'Holiday _Flag', 'Temprature', 'Fuel_Price', 'CPI', 'Unemployment']
+# Make predictions on the test set using both models
+y_pred_rf = rf_model.predict(x_test)
+y_pred_xgb = xgb_model.predict(x_test)
+y_pred_gb = gb_model.predict(x_test)
 
-# Make predictions on the test data
-for index, row in test_data.iterrows():
-    # Extract features from the current row
-    features = row[feature_columns].values.reshape(1, -1)
 
-    # Make a prediction using the trained model
-    vals.append(model.predict(features))
+# Calculate and print the Root Mean Squared Error (RMSE) for both models
+rmse_rf = float(format(np.sqrt(mean_squared_error(y_test, y_pred_rf)), '.3f'))
+rmse_xgb = float(format(np.sqrt(mean_squared_error(y_test, y_pred_xgb)), '.3f'))
+rmse_gb = float(format(np.sqrt(mean_squared_error(y_test, y_pred_gb)), '.3f'))
+print("\nRandom Forest RMSE: ", rmse_rf)
+print("XGBoost RMSE: ", rmse_xgb)
+print("Gradient Boost RMSE: ", rmse_gb)
 
-print(vals)
-
-# Display the predictions along with the corresponding dates
-for x in range(len(vals)):
-    amnt = str(vals[x]).replace('[', '').replace(']', '')
-    print("Date: ", dates[x], "\tPredicted Income: $", amnt)
+# Random Forest RMSE:  150619.75
+# XGBoost RMSE:  142014.462
+# Gradient Boost RMSE:  127761.866
